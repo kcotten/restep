620b1f4a593ae5d72e13fcfbd0aeeb8004ec9a67
kcotten <kris.cotten@gmail.com>
2022-09-07 12:01:17 -0700

    init shell to pull down coverage badge

---
+++ bin/cov.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# TODO: CI to generate coverage badge
+# Generate coverage report and then generate badge
+
+set -euo pipefail
+
+go test -v ./... -covermode=count -coverprofile=coverage.out
+go tool cover -func=coverage.out -o=coverage.out
+RESULT=$(< coverage.out grep total | awk '{gsub(/%/,""); print $3}')
+COLOR=GREEN # static color for now
+curl "https://img.shields.io/badge/coavrege-$RESULT%25-$COLOR" > badge.svg
